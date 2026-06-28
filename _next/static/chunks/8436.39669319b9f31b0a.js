2121022e1bc6d04b12aeb6f32876a72460299bc5
Phoebe Chao <train981316@gmail.com>
2026-06-28 23:09:05 +0100

    Deploy static site

---
+++ _next/static/chunks/8436.39669319b9f31b0a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8436],{83686:()=>{},32383:()=>{}}]);
---
+++ _next/static/tIM97psD8c5gN83idA17s/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fcontact","\u002F[locale]\u002Fguides","\u002F[locale]\u002Fguides\u002F[slug]","\u002F[locale]\u002Fprivacy","\u002F[locale]\u002Fterms","\u002F[locale]\u002Ftools\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
